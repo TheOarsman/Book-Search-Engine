9ce604d15a53119e41546ecf5707efffe199d0fe
TheOarsman <heinzulrichv@gmail.com>
2024-03-18 11:40:01 -0400

    apollo server logic added

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const db = require("./config/connection");
+const typeDefs = require("./schemas");
+const resolvers = require("./resolvers");
+const { authMiddleware } = require("./middleware/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function startApolloServer() {
+  // Create an instance of ApolloServer with type definitions and resolvers
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      // Pass the request object to the context to access it in resolvers
+      return { req };
+    },
+  });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Apply ApolloServer instance as middleware to Express
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.use(authMiddleware);
+
+  await db.once("open", () => {
+    console.log("Connected to the database");
+  });
+
+  app.listen(PORT, () => {
+    console.log(
+      `🌍 Server listening on http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
 }
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Start Apollo Server and Express
+startApolloServer();
